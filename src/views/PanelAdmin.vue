10e5d65b053782546b4c1682764d1085fcbee0bf
bitameshgi <89532084+bitameshgi@users.noreply.github.com>
2021-09-30 16:20:58 +0330

    last change !

--- src/views/PanelAdmin.vue
+++ src/views/PanelAdmin.vue
@@ -1,127 +1,84 @@
 <template>
-    <div >
-        <Navbar/>
-        <v-col cols="8">
-            <h2 class="ti-post"> Create Post: </h2>
+  <div>
+    <Navbar />
+    <v-col cols="8">
+      <h2 class="ti-post">Create Post:</h2>
 
-        <v-form class="post-fo"
-       
-    
-   
-    
-  >
-   <label> Title</label>
-    <v-text-field 
-     ref="Title"
-      v-model="Title"
-      :counter="10"
-   @input="value => Title=value"
-      
-      outlined
-      required
-    ></v-text-field>
- <label> Body</label>
-    <v-textarea 
-          
-       ref="Body"
-      v-model="Body"
-      @input="value => Body=value"
-      outlined
-      required
-    >
-</v-textarea>
+      <v-form class="post-fo"  @submit.prevent="validate">
+        <label> Title</label>
+        <v-text-field
+          v-model="Title"
+          :counter="10"
+          outlined
+          required
+        ></v-text-field>
+        <label> Body</label>
+        <v-textarea v-model="Body" outlined required> </v-textarea>
 
-
-    <v-btn
-      
-     
-       @submit.prevent="validate"
-    >
-     Create Post
-    </v-btn>
-
-  </v-form>
-        </v-col>
-    </div>
-
-
+        <v-btn type="submit" > Create Post </v-btn>
+      </v-form>
+    </v-col>
+  </div>
 </template>
 
 
 <script>
-import axios from 'axios'
-import Navbar from '../components/Navbar.vue'
+import axios from "axios";
+import Navbar from "../components/Navbar.vue";
 export default {
-    components:{
-        Navbar
-    },
-    created(){
+  components: {
+    Navbar,
+  },
+  created() {
     //     const form = new FormData();
-    //     axios.post("/posts", form,{ 
+    //     axios.post("/posts", form,{
     //         title:form.title,
     //         body:form.body,
-           
     //     })
     //    .then(Response => {
     //         console.log(Response.data)
-         
     //     })
-    },
-     data: () => ({
-         
-      valid: true,
-      Title: '',
-      TitleRules: [
-        v => !!v || 'Title is required',
-        v => (v && v.length <= 10) || 'Title must be less than 10 characters',
-      ],
-      Body: '',
-      BodyRules: [
-        v => !!v || 'Body is required',
-        v => /.+@.+\..+/.test(v) || 'Body must be valid',
-      ],
-         
-}),
-// data(){
-//     return{
-        
-//             Title:'',
-//         Body:''
-        
-//     }
-// },
-
-    methods: {
-     
+  },
+  data: () => ({
+    valid: true,
+    Title: "",
+    TitleRules: [
+      (v) => !!v || "Title is required",
+      (v) => (v && v.length <= 10) || "Title must be less than 10 characters",
+    ],
+    Body: "",
+    BodyRules: [
+      (v) => !!v || "Body is required",
+      (v) => /.+@.+\..+/.test(v) || "Body must be valid",
+    ],
+  }),
+  methods: {
     //   reset () {
     //     this.form.reset()
     //   },
-    validate(){
-const newvalidate={
-    "Title": this.title,
-    "Body": this.Body
-}
-axios.post(`/posts`, newvalidate)
-.then(Response=> {
-    console.log(Response.data);
-    console.log(newvalidate);
-})
-  .catch(error=> console.log(error)) 
-
-    }
-    }
-     
-}
-
-
+    validate() {
+      const newvalidate = {
+        Title: this.Title,
+        Body: this.Body,
+      };
+      axios
+        .post(`/posts`, newvalidate)
+        .then((Response) => {
+          console.log(Response.data);
+          console.log(newvalidate);
+        })
+        .catch((error) => console.log(error));
+    },
+  },
+};
 </script>
 <style scoped>
-.ti-post{
-    padding: 20px;
+.ti-post {
+  padding: 20px;
 }
 </style>
 <style scoped>
-.post-fo{
-    padding: 20px;
+.post-fo {
+  padding: 20px;
 }
 </style>
